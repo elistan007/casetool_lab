dfb4401865e1d485ce34948f30dd9eda47d5f15c
exam <exam@Bcalab4-m20>
2023-05-03 09:31:07 +0530

    commit2

--- casetoolExam/src/casetoolExam/Car_122.java
+++ casetoolExam/src/casetoolExam/Car_122.java
@@ -1,10 +1,10 @@
 package casetoolExam;
 
 public class Car_122 {
-	int speed;
+	int speeds;
 	Car_122(int speed)
 	{
-		this.speed=speed;
+		this.speeds=speed;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
